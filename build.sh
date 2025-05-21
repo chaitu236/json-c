bff786458f3415bb514f820dab0f8b15f9592a03
Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>
2025-05-21 11:10:47 -0500

    USE_PTHREADS=1

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ mkdir json-c-build
 
 cd json-c-build
 emcmake ../cmake-configure -- -DCMAKE_INSTALL_PREFIX=/tmp/emscripten_root
-emmake make
+EMCC_CFLAGS="-s USE_PTHREADS=1" emmake make
+emmake make install
