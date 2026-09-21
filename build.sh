695ed4a5ce964837c9926e5b8ee5f259f53c5c06
Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>
2026-09-20 22:53:16 -0500

    build.sh: Make it better

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-rm -rf json-c-build
-mkdir json-c-build
 
-cd json-c-build
-emcmake ../cmake-configure -- -DCMAKE_INSTALL_PREFIX=/tmp/emscripten_root
-EMCC_CFLAGS="-s USE_PTHREADS=1" emmake make
+set -e
+
+ROOT=$(cd "$(dirname "$0")" && pwd)
+PREFIX=${PREFIX:-/tmp/emscripten_root}
+BUILD_DIR=${BUILD_DIR:-"$ROOT/json-c-build"}
+export EMCC_CFLAGS="${EMCC_CFLAGS:+$EMCC_CFLAGS }-s USE_PTHREADS=1"
+
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+emcmake "$ROOT/cmake-configure" -- \
+	-DCMAKE_INSTALL_PREFIX="$PREFIX" \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DBUILD_STATIC_LIBS=ON \
+	-DBUILD_APPS=OFF \
+	-DBUILD_TESTING=OFF
+emmake make
 emmake make install
